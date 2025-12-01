a50e6570ce498870801d25b415e9b7297202d171
keithrbennett <keithrbennett@gmail.com>
2025-12-01 22:16:36 +0800

    Add spec file to repo that should have been added previously.

---
+++ spec/cli_json_options_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe SimpleCovMcp::CoverageCLI, 'json format options' do
+  let(:root) { (FIXTURES_DIR / 'project1').to_s }
+
+  def run_cli_output(*argv)
+    cli = SimpleCovMcp::CoverageCLI.new
+    output = nil
+    silence_output do |stdout, _stderr|
+      cli.send(:run, argv)
+      output = stdout.string
+    end
+    output
+  end
+
+  describe 'JSON format options' do
+    it 'produces compact JSON with -f j' do
+      output = run_cli_output('--root', root, '--resultset', 'coverage', '-f', 'j', 'list')
+
+      expect(output.strip.lines.count).to eq(1)
+      data = JSON.parse(output)
+      expect(data['files']).to be_an(Array)
+    end
+
+    it 'produces pretty JSON with -f pretty-json' do
+      output = run_cli_output('--root', root, '--resultset', 'coverage', '-f', 'pretty-json',
+        'list')
+      expect(output.strip.lines.count).to be > 1
+      data = JSON.parse(output)
+      expect(data['files']).to be_an(Array)
+    end
+
+    it 'produces pretty JSON with -f pretty_json (underscore variant)' do
+      output = run_cli_output('--root', root, '--resultset', 'coverage', '-f', 'pretty_json',
+        'list')
+      expect(output.strip.lines.count).to be > 1
+      data = JSON.parse(output)
+      expect(data['files']).to be_an(Array)
+    end
+
+    it 'produces compact JSON with -f json' do
+      output = run_cli_output('--root', root, '--resultset', 'coverage', '-f', 'json', 'list')
+      expect(output.strip.lines.count).to eq(1)
+      data = JSON.parse(output)
+      expect(data['files']).to be_an(Array)
+    end
+  end
+end
